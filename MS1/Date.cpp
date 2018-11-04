3a4bba0868da4cb3d5812040a7e654e7f693eaa6
Kashyap Bharda <kashyapbharda12645@gmail.com>
2018-11-03 23:02:48 -0400

    Read, Write Override Function

--- MS1/Date.cpp
+++ MS1/Date.cpp
@@ -1,15 +1,29 @@
 // Final Project Milestone 1
 //
 // Version 1.0
-// Date
-// Author
-// Description
+// Date 11/03/2018
+// Author Kashyap Bharda
+// Description Milestone 1
 //
 // Revision History
 // -----------------------------------------------------------
 // Name               Date                 Reason
 /////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <ostream>
+#include <iomanip>
 #include "Date.h"
+
+using namespace std;
+using namespace aid;
+
+#define NO_ERROR   0  
+#define CIN_FAILED 1  
+#define DAY_ERROR  2  
+#define MON_ERROR  3  
+#define YEAR_ERROR 4  
+#define PAST_ERROR 5  
 
 namespace aid {
 
@@ -22,6 +36,192 @@ namespace aid {
 		return days[month] + int((month == 1)*((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
 	}
 
+	void Date::errCode(int errorCode)
+	{
+		this->errorState = errorCode;
+	}
+
+	bool Date::is_empty() const
+	{
+		if (year == 0 && month == 0 && days == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	Date::Date()
+	{
+		this->year = 0;
+		this->month = 0;
+		this->days = 0;
+		this->errorState = NO_ERROR;
+		this->comparator = 0;
+	}
+
+	Date::Date(int year, int month, int day)
+	{
+		if (year < min_year || year > max_year) {
+			this->errCode(YEAR_ERROR);
+		}
+		else if (month < 1 || month > 12) {
+			this->errCode(MON_ERROR);
+		}
+		else if (day < 1 || day > this->mdays(month, year)) {
+			this->errCode(DAY_ERROR);
+		}
+		else {
+			this->year = year;
+			this->month = month;
+			this->days = day;
+			this->errCode(NO_ERROR);
+			this->comparator = year * 372 + month * this->mdays(month, year) + day;
+
+		}
+
+		if (this->errorState != NO_ERROR) {
+			this->year = 0;
+			this->month = 0;
+			this->days = 0;
+			this->errorState = NO_ERROR;
+			this->comparator = 0;
+		}
+
+	}
+
+	int Date::errCode() const
+	{
+		return this->errorState;
+	}
+
+	bool Date::bad() const
+	{
+		if (this->errorState != NO_ERROR) {
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool Date::operator==(const Date & rhs) const
+	{
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year == rhs.year && this->month == rhs.month && this->days == rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	bool Date::operator!=(const Date & rhs) const
+	{
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year != rhs.year && this->month != rhs.month && this->days != rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+
+	}
+
+	bool Date::operator<(const Date & rhs) const
+	{
+
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year < rhs.year && this->month < rhs.month && this->days < rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	bool Date::operator>(const Date & rhs) const
+	{
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year > rhs.year && this->month > rhs.month && this->days > rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	bool Date::operator<=(const Date & rhs) const
+	{
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year <= rhs.year && this->month <= rhs.month && this->days <= rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	bool Date::operator>=(const Date & rhs) const
+	{
+		if (!this->is_empty() && !rhs.is_empty())
+		{
+			return this->year >= rhs.year && this->month >= rhs.month && this->days >= rhs.days;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	std::istream& Date::read(std::istream & istr)
+	{
+		// TODO: insert return statement here
+		char dash;
+
+		istr >> this->year >> dash;
+		istr >> this->month >> dash;
+		istr >> this->days;
+
+		if (istr.fail())
+		{
+			errCode(CIN_FAILED);
+			return istr;
+		}
+		return istr;
+	}
+
+	std::ostream& Date::write(std::ostream & ostr) const
+	{
+
+		ostr << this->year << "/" << this->month << "/" << this->days << std::endl;
+		return ostr;
+	}
+
+	std::istream& operator>>(std::istream & istr, Date & rhs)
+	{
+		rhs.read(istr);
+		return istr;
+	}
+
+	std::ostream & operator<<(std::ostream & ostr, Date & rhs)
+	{
+
+		rhs.write(ostr);
+		return ostr;
+	}
 
 
 }
+
