cade1ddda3a0b65eeaa65f520aaaa0f7eeaac6b3
Kashyap Bharda <kashyapbharda12645@gmail.com>
2018-11-24 01:21:39 -0500

    m4

--- MS4/iGood.h
+++ MS4/iGood.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#ifndef AMA_IGOOD_H
+#define AMA_IGOOD_H
+
+#include <iostream>
+#include <fstream>
+
+namespace ama {
+
+	class iGood {
+		// PUBLIC MEMBERS
+
+	public:
+
+		virtual std::fstream& store(std::fstream& file, bool newLine = true) const = 0;
+		virtual std::fstream& load(std::fstream& file) = 0;
+		virtual std::ostream& write(std::ostream& os, bool linear) const = 0;
+		virtual std::istream& read(std::istream& is) = 0;
+
+		virtual bool operator==(const char*) const = 0;
+		virtual double total_cost() const = 0;
+		virtual const char* name() const = 0;
+		virtual void quantity(int) = 0;
+		virtual int qtyNeeded() const = 0;
+		virtual int quantity() const = 0;
+		virtual int operator+=(int) = 0;
+		virtual bool operator>(const iGood&) const = 0;
+
+		virtual ~iGood() {}
+	};
+
+	std::ostream& operator<<(std::ostream&, const iGood&);
+	std::istream& operator>>(std::istream&, iGood&);
+
+	double operator+=(double&, const iGood&);
+
+	iGood* CreateGood();
+	iGood* CreatePerishable();
+}
+
+#endif // !AMA_IGOOD_H
