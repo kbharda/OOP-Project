ac8acf06f6f49de4c26094e165b8b48e5f8f5d46
Kashyap Bharda <kashyapbharda12645@gmail.com>
2018-11-24 00:41:38 -0500

    ms6

--- MS4/ms4_MyGood.h
+++ MS4/ms4_MyGood.h
@@ -18,32 +18,34 @@
 // Chris              2018-03-10           Matching specs
 // Chris              2018-10-18           fall 2018
 /////////////////////////////////////////////////////////////////
-#ifndef AID_MY_GOOD_H
-#define AID_MY_GOOD_H
+
+#ifndef AID_MYGOOD_H
+#define AID_MYGOOD_H
 #include "iGood.h"
 #include "iGood.h"
 #include "iGood.h" // Good.h is included three times on purpose.
+#include "iProduct.h"
 
 namespace aid {
-  class MyGood : public iGood {
-    char text_[10000];
-	char sku[4];
-	char name_[20];
-	int value;
-  public:
-    MyGood(const char* name);
-    std::fstream& store(std::fstream& file, bool addNewLine)const;
-    std::fstream& load(std::fstream& file);
-    std::ostream& write(std::ostream& os, bool linear)const;
-    std::istream& read(std::istream& is);
-	bool operator==(const char*) const;
-	double total_cost() const;
-	const char* name() const;
-	void quantity(int);
-	int qtyNeeded() const;
-	int quantity() const;
-	int operator+=(int);
-	bool operator>(const iGood&) const;
-  };
+	class MyGood : public iGood {
+		char text_[10000];
+		char sku[4];
+		char name_[20];
+		int value;
+	public:
+		MyGood(const char* name);
+		std::fstream& store(std::fstream& file, bool addNewLine)const;
+		std::fstream& load(std::fstream& file);
+		std::ostream& write(std::ostream& os, bool linear)const;
+		std::istream& read(std::istream& is);
+		bool operator==(const char*) const;
+		double total_cost() const;
+		const char* name() const;
+		void quantity(int);
+		int qtyNeeded() const;
+		int quantity() const;
+		int operator+=(int);
+		bool operator>(const iGood&) const;
+	};
 }
-#endif
+#endif // !AID_MYGOOD_H
